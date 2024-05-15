f752d6446f817a734c5a832446eb482452925966
Ramon-Ariel-ET12 <Ramonarielet12d1@gmail.com>
2024-05-14 21:43:33 -0300

    Completado (No encontre el problema que habia dicho)

--- Poker12.Core.Test/Jugadas/EscaleraTest.cs
+++ Poker12.Core.Test/Jugadas/EscaleraTest.cs
@@ -62,6 +62,38 @@ public class EscaleraTest
         Assert.Equal(14, resultado.Valor);
     }
     [Fact]
+    public void CorrectoEscaleraParte2()
+    {
+        var jugada = new List<Carta>()
+        {
+            new(EPalo.Corazon, EValor.Nueve),
+            new(EPalo.Diamante, EValor.Diez),
+            new(EPalo.Trebol, EValor.Q),
+            new(EPalo.Corazon, EValor.J),
+            new(EPalo.Picas, EValor.K),
+        };
+        var resultado = escalera.Aplicar(jugada);
+
+        Assert.Equal(13, resultado.Valor);
+    }
+
+    [Fact]
+    public void IncorrectoEscaleraParte2()
+    {
+        var jugada = new List<Carta>()
+        {
+            new(EPalo.Corazon, EValor.Nueve),
+            new(EPalo.Diamante, EValor.Diez),
+            new(EPalo.Trebol, EValor.Q),
+            new(EPalo.Corazon, EValor.K),
+            new(EPalo.Picas, EValor.As),
+        };
+        var resultado = escalera.Aplicar(jugada);
+
+        Assert.Equal(0, resultado.Valor);
+    }
+
+    [Fact]
     public void IncorrectoEscalera()
     {
         var jugada = new List<Carta>()
@@ -71,6 +103,21 @@ public class EscaleraTest
             new(EPalo.Diamante, EValor.As),
             new(EPalo.Trebol, EValor.Q),
             new(EPalo.Corazon, EValor.K),
+        };
+        var resultado = escalera.Aplicar(jugada);
+
+        Assert.Equal(0, resultado.Valor);
+    }
+    [Fact]
+    public void VariosAS()
+    {
+        var jugada = new List<Carta>()
+        {
+            new(EPalo.Trebol, EValor.As),
+            new(EPalo.Picas, EValor.As),
+            new(EPalo.Corazon, EValor.As),
+            new(EPalo.Diamante, EValor.K),
+            new(EPalo.Corazon, EValor.Tres),
         };
         var resultado = escalera.Aplicar(jugada);
 
