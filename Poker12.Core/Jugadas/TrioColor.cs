dd0241614a5484e00d3ce2314b9c48731d642ad8
David3578 <128637961+David3578@users.noreply.github.com>
2024-05-18 17:37:34 -0300

    terminando el trio color

--- Poker12.Core/Jugadas/TrioColor.cs
+++ Poker12.Core/Jugadas/TrioColor.cs
@@ -7,23 +7,12 @@ public class TrioColor : IJugada
     {
         if (cartas.Count < 3)
             throw new ArgumentException("Necesitas al menos 3 cartas para jugar un Trio Color");
-        var cartaByte = cartas.Select(c => (byte)c.Valor).ToList(); 
-        var counts = new Dictionary<byte, int>();
-        foreach (var valor in cartaByte)
+        var tripleValue = cartas.GroupBy(c => c.Valor).FirstOrDefault(g => g.Count() == 3)?.Key;
+        if (tripleValue.HasValue)
         {
-            if (!counts.ContainsKey(valor))
-                counts[valor] = 1;
-            else
-                counts[valor]++;
-        }
-        foreach (var count in counts.Values)
-        {
-            if (count == 3)
-            {
-                var highestValue = counts.FirstOrDefault(pair => pair.Value == 3).Key;
-                return new Resultado(Prioridad, highestValue == 1? (byte)14 : highestValue); 
-            }
+            return new Resultado(Prioridad, tripleValue.Value == EValor.As ? (byte)14 : (byte)tripleValue.Value);
         }
         return new Resultado(Prioridad, 0); 
     }
 }
+
