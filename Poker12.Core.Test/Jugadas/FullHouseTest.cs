73062387a071fc3a04506c8f2014bdba7bfaf256
FranciscoBHF <114776184+FranciscoBHF@users.noreply.github.com>
2024-05-20 16:12:12 -0300

    actualizacion al test de full house
    Se agregaron 2 test mas

--- Poker12.Core.Test/Jugadas/FullHouseTest.cs
+++ Poker12.Core.Test/Jugadas/FullHouseTest.cs
@@ -29,4 +29,36 @@ public class FullHouseTest
 
         Assert.Equal(10, resultado.Valor);
     }
+    [Fact]
+    public void PruebaConFalla()
+    {
+        var jugada = new List<Carta>()
+        {
+            new(EPalo.Picas, EValor.Diez),
+            new(EPalo.Trebol, EValor.Diez),
+            new(EPalo.Diamante, EValor.Tres),
+            new(EPalo.Trebol, EValor.As),
+            new(EPalo.Corazon, EValor.As)
+        };
+
+        var resultado = _FullHouse.Aplicar(jugada);
+
+        Assert.Equal(0, resultado.Valor);
+    }
+    [Fact]
+    public void PruebaConDos()
+    {
+        var jugada = new List<Carta>()
+        {
+            new(EPalo.Picas, EValor.Cinco),
+            new(EPalo.Trebol, EValor.Cinco),
+            new(EPalo.Diamante, EValor.Cinco),
+            new(EPalo.Trebol, EValor.Dos),
+            new(EPalo.Corazon, EValor.Dos)
+        };
+
+        var resultado = _FullHouse.Aplicar(jugada);
+
+        Assert.Equal(5, resultado.Valor);
+    }
 }
