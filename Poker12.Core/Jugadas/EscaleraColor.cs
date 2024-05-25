693ae56a5fa188efd935ae303a79cb0a367b7e95
Zein2006 <davidbzk95@gmail.com>
2024-05-24 22:16:31 -0300

    Completado escaleras

--- Poker12.Core/Jugadas/EscaleraColor.cs
+++ Poker12.Core/Jugadas/EscaleraColor.cs
@@ -13,35 +13,4 @@ public class EscaleraColor : JugadaAbs
         }
         return base.Aplicar(cartas);
     }
-    // public string Nombre => "Escalera de color";
-    // public byte Prioridad => 2;
-    // public Resultado Aplicar(List<Carta> cartas)
-    // {
-    //     if (cartas.Count < 5)
-    //     {
-    //         throw new ArgumentException("No se puede realizar esta jugada");
-    //     }
-
-    //     bool sonCorazones = cartas.All(x => x.Palo == EPalo.Corazon);
-    //     bool sonDiamantes = cartas.All(x => x.Palo == EPalo.Diamante);
-    //     bool sonPicas = cartas.All(x => x.Palo == EPalo.Picas);
-    //     bool sonTreboles = cartas.All(x => x.Palo == EPalo.Trebol);
-
-    //     if (sonCorazones || sonDiamantes || sonPicas || sonTreboles)
-    //     {
-    //         List<byte> cartasByte;
-    //         List<byte> valoresByte = Enumerable.Range(1, 13).Select(x => (byte)x).ToList();
-    //         var ordenadasPorValor = cartas.OrderBy(x => x.Valor).ToList();
-
-    //         cartasByte = ordenadasPorValor.Select(x => (byte)x.Valor).ToList();
-
-    //         var recortarValoresByte = valoresByte.Where(x => x >= cartasByte.First() && x <= cartasByte.Last()).ToList();
-
-    //         var valor = recortarValoresByte.Count == 5 ? (byte)ordenadasPorValor.Last().Valor : (byte)0; 
-
-    //         return new Resultado(Prioridad, valor);
-    //     }
-
-    //     return new Resultado(Prioridad, (byte)0);
-    // }
 }
