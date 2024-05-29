8a78fa15db89603c0ecdad132a63bfb42befc986
NoeliaAT <114776463+NoeliaAT@users.noreply.github.com>
2024-05-29 14:43:13 -0300

    Refactor Trio (#19)
    * Terminado Trio

--- Poker12.Core/Jugadas/Trio.cs
+++ Poker12.Core/Jugadas/Trio.cs
@@ -1,18 +1,15 @@
 namespace Poker12.Core.Jugadas;
-public class Trio : IJugada
+public class Trio : JugadaAbs
 {
-    public string Nombre => "Trio Color";
-    public byte Prioridad => 7; 
-    public Resultado Aplicar(List<Carta> cartas)
+    public Trio() : base ("Trio", 7) {}
+    
+    protected override Resultado Aplicar(CartasJugada cartas)
     {
-        if (cartas.Count < 3)
-            throw new ArgumentException("Necesitas al menos 3 cartas para jugar un Trio Color");
-        var groups = cartas.GroupBy(c => c.Valor).Where(g => g.Count() == 3);
-        if (groups.Any())
-        {
-            var highestValue = groups.Max(g => g.Key);
-            return new Resultado(Prioridad, highestValue == EValor.As? (byte)14 : (byte)highestValue);
-        }
-        return new Resultado(Prioridad, 0); 
+        var valorTrio = cartas.MayorValorConNCartas(3);
+        if(valorTrio != 0)
+            return ResultadoCon(valorTrio);
+        
+        return base.Aplicar(cartas);
+        
     }
 }
