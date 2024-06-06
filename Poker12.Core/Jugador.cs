2a6050408c779ba5592b0e5803b71658d4cae2dd
JRyoy <josbert0708@gmail.com>
2024-06-05 21:07:11 -0300

    Listo

---
+++ Poker12.Core/Jugador.cs
@@ -0,0 +1,26 @@
+namespace Poker12.Core;
+
+public class Jugador
+{
+    public string Nombre { get; set; }
+    public Carta? carta1 { get; set; }
+    public Carta? carta2 { get; set; }
+    public ushort Fichas { get; private set; }
+
+    public Jugador(string Nombre, ushort Fichas)
+    {
+        this.Nombre = Nombre;
+        this.Fichas = Fichas;
+    }
+    public bool PuedeApostar(ushort FichasMesa)
+        => Fichas >= FichasMesa;
+    public void SacarFichas(ushort FichasMesa)
+    {
+        if (!PuedeApostar(FichasMesa))
+            throw new InvalidOperationException();
+            
+        Fichas-=FichasMesa;
+    }
+    public void AcreditarFichas(ushort FichasMesa)
+    =>Fichas+=FichasMesa;
+}
