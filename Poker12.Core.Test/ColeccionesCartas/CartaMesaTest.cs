432681392a45faae0a2808b7cdcd790e27ca9c45
luchsj23 <>
2024-06-06 15:49:52 -0300

    Updated

---
+++ Poker12.Core.Test/ColeccionesCartas/CartaMesaTest.cs
@@ -0,0 +1,11 @@
+using Poker12.Core.ColeccionesCartas;
+namespace Poker12.Core.Test.ColeccionesCartas;
+
+public class CartaMesaTest
+{
+    [Fact]
+    public void Test1()
+    {
+        
+    }
+}
