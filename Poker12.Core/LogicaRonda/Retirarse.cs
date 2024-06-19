239a03ae9e818cc6f3bb34796b9902518c4dd3bd
FranciscoBHF <114776184+FranciscoBHF@users.noreply.github.com>
2024-06-19 15:49:56 -0300

    Retirarse fue creado
    La clase para que el jugador se retire de una ronda fue creada

---
+++ Poker12.Core/LogicaRonda/Retirarse.cs
@@ -0,0 +1,10 @@
+namespace Poker12.Core.LogicaRonda
+{
+    public class Retirarse(Jugador jugador, Ronda ronda, ushort apuesta) : RespuestaJugadorARonda(jugador, ronda, apuesta)
+    {
+        public override void Ejecutar()
+        {
+            jugador.Activo = false;
+        }
+    }
+}
