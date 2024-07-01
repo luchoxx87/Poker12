fc573d820f3c5a7819d6d0d30fa96ce7c6b8f54e
Tu Nombre <axeleonelmtz8@gmail.com>
2024-07-01 15:38:51 -0300

    Update Mantener.cs

--- Poker12.Core/LogicaRonda/Mantener.cs
+++ Poker12.Core/LogicaRonda/Mantener.cs
@@ -2,6 +2,9 @@ namespace Poker12.Core.LogicaRonda;
 public class Mantener(Jugador jugador, Ronda ronda, ushort apuesta)
     : RespuestaJugadorARonda(jugador, ronda, apuesta)
 {
-    public override void Ejecutar()=>
+    public override void Ejecutar()
+    {
         jugador.SacarFichas(apuesta);
+        ronda.IncrementarPozo(apuesta);
+    }
 }
