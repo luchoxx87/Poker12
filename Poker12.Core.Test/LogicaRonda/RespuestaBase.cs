d27def87ad149c8ad533555013ec0fe0284d186f
Luis Duran <neoluis87@gmail.com>
2024-07-03 15:29:41 -0300

    Agregando caso base para los test de respuestas

---
+++ Poker12.Core.Test/LogicaRonda/RespuestaBase.cs
@@ -0,0 +1,17 @@
+using Poker12.Core.ColeccionesCartas;
+
+namespace Poker12.Core.LogicaRonda.Test;
+internal class RespuestaBase
+{
+    protected Ronda ronda;
+    protected Jugador Tomy;
+    protected ushort ApuestaInicial = 500;
+    protected readonly IMezclador Mezclador = new NoMezcla();
+    public RespuestaBase()
+    {
+        Tomy = new Jugador("Tomy", 100);
+        var mazo = new Mazo(Mezclador.ObtenerCartas());
+        ronda = new Ronda(ApuestaInicial, [Tomy], mazo);
+        Tomy.Activar();
+    }
+}
